b7cbcb3ddc463f3bfdf2b29948d58aa7d35cb287
hamoodahalabed <99316589+hamoodahalabed@users.noreply.github.com>
2023-05-15 17:53:24 +0300

    level 4 (need some change)
    add real player
    change the speed of player or boss
    fix castle sprite

---
+++ platformer project/Assets/sprites/level 4/trash/player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float moveSpeed = 2.5f;
+    public float jumpForce = 2.5f;
+    private bool isJumping = false;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+}
